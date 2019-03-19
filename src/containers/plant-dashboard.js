bc1a178c235058cdc341ecc9e9ca66a33a593d5d
Daniel Rojas <crimsondex@yahoo.com>
2019-03-19 11:54:59 -0700

    Added fetch functionality to PlantDashboard component.

--- src/containers/plant-dashboard.js
+++ src/containers/plant-dashboard.js
@@ -1,7 +1,26 @@
 import * as React from 'react'
+import { setInterval } from 'core-js';
 
-const PlantDashboard = () => {
-  return <div>Plant Dashboard</div>
+class PlantDashboard extends React.PureComponent {
+  componentDidMount() {
+    this.timer = setInterval(() => this.getPlantData(), 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+    this.timer = null
+  }
+
+  getPlantData() {
+    fetch("http://demo.etechsystems.com/interview_problem_data.json")
+      .then(res => res.json())
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return <div>Plant Dashboard</div>
+  }
 }
 
 export default PlantDashboard
