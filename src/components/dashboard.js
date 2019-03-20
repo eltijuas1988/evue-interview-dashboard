bc624521e90a737ea2ca7885397ffdf485b3b987
Daniel Rojas <crimsondex@yahoo.com>
2019-03-20 10:08:30 -0700

    Wrapped up in redux store Provider.

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,17 @@
 import * as React from 'react'
+import {Provider} from 'react-redux'
 import {PlantDashboard} from '../containers'
+import configureStore from '../store'
 
 const Dashboard = () => {
+  const store = configureStore()
+
   return (
-    <div><PlantDashboard/></div>
+    <div>
+      <Provider store={store}>
+        <PlantDashboard/>
+      </Provider>
+    </div>
   )
 }
 
