6e3c3041bc78b629cf5fc4e46cf57ec2b7930e67
Daniel Rojas <crimsondex@yahoo.com>
2019-03-21 11:51:43 -0700

    Removed padding from top of App.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import {DARK_PURPLE} from './constants'
 const App = ({classes}) => {
   return (
     <div className={classes.root}>
-      <div className={classes.header}>
-        <Header/>
-      </div>
+      <Header/>
       <div className={classes.dashboard}>
         <Dashboard/>
       </div>
@@ -19,9 +17,6 @@ const App = ({classes}) => {
 const styles = {
   root: {
     backgroundColor: DARK_PURPLE,
-  },
-  header: {
-    paddingTop: 25,
   },
   dashboard: {
     padding: 25,
