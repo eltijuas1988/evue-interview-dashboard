c3687252627162f0b1b2c4cbca567d8cb7688bb2
Daniel Rojas <crimsondex@yahoo.com>
2019-03-21 12:19:31 -0700

    Added prop color in styles that calls function and determines the text color based on efficiency.

--- src/components/Machine.js
+++ src/components/Machine.js
@@ -1,23 +1,11 @@
 import * as React from 'react'
+import injectSheet from 'react-jss'
 
 const Machine = (props) => {
-  const {id, name, efficiency, fault, type} = props
-  let color = "red"
-
-  if (efficiency >= 80) {
-    color = "green"
-
-  } else if (efficiency > 60 && efficiency < 80) {
-    color = "yellow"
-  }
-
-  const styles = {
-    ...equipmentStyle,
-    color,
-  }
+  const {classes, id, name, efficiency, fault, type} = props
 
   return (
-    <div key={id} style={styles}>
+    <div className={classes.root}>
       <div>{id}</div>
       <div>{name}</div>
       <div>{efficiency}</div>
@@ -27,12 +15,28 @@ const Machine = (props) => {
   )
 }
 
-const equipmentStyle = {
-  width: 100,
-  height: 100,
-  padding: 10,
-  backgroundColor: "black",
-  margin: 5,
+const determineEfficiencyColor = ({efficiency}) => {
+  let color = "red"
+
+  if (efficiency >= 80) {
+    color = "green"
+
+  } else if (efficiency > 60 && efficiency < 80) {
+    color = "yellow"
+  }
+
+  return color
 }
 
-export default Machine
+const styles = {
+  root: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    color: props => determineEfficiencyColor({efficiency: props.efficiency}),
+    backgroundColor: "black",
+    margin: 5,
+  },
+}
+
+export default injectSheet(styles)(Machine)
