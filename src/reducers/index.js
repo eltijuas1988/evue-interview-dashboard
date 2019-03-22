17da76ce334d210497f40229e903fad1e06e4679
Daniel Rojas <crimsondex@yahoo.com>
2019-03-22 10:22:53 -0700

    Root reducer is created from Immer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux'
+import produce from 'immer'
 import areas from './areas'
 
-const reducer = combineReducers({areas})
+const reducer = produce(combineReducers({areas}), draftState => draftState)
 
 export default reducer
