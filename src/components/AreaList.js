338270b3f387b9cdd52ba5dae7f73cfce3175a65
Daniel Rojas <crimsondex@yahoo.com>
2019-03-22 11:50:21 -0700

    Set conditional statements for what renders based on incoming props.

--- src/components/AreaList.js
+++ src/components/AreaList.js
@@ -1,18 +1,29 @@
 import * as React from 'react'
 import injectSheet from 'react-jss'
 import {TEAL} from '../constants'
+import {CircularProgress} from '../material-ui'
 import Machine from './Machine'
 
 const AreaList = ({classes, title, machines, type}) => {
-  if (!machines) return null
+  let rendered
+  if (machines === undefined) {
+    rendered =
+      <div className={classes.progressWrapper}><CircularProgress/></div>
+
+  } else if (machines && machines.length < 1) {
+    rendered = <div className={classes.noDataText}>No Data Available</div>
+
+  } else {
+    rendered = machines.filter(m => m.type === type).map(machine => {
+      return <Machine key={machine.id} {...machine}/>
+    })
+  }
 
   return (
     <div className={classes.root}>
       <div className={classes.title}>{title}</div>
       <div className={classes.area}>
-        {machines.filter(m => m.type === type).map(machine => {
-          return <Machine key={machine.id} {...machine}/>
-        })}
+        {rendered}
       </div>
     </div>
   )
@@ -27,7 +38,18 @@ const styles = {
     color: TEAL,
   },
   area: {
-    display: 'flex',
+    display: "flex",
+  },
+  noDataText: {
+    color: "red",
+    fontSize: 20,
+    width: "100%",
+    textAlign: "center",
+  },
+  progressWrapper: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
   },
 }
 
