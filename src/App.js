1c350ab9177532b259c6fdb787cc5ca464b9cd2e
Daniel Rojas <crimsondex@yahoo.com>
2019-03-23 09:27:15 -0700

    Removed App.css and moved styles as JSS to App.js.

--- src/App.js
+++ src/App.js
@@ -21,6 +21,37 @@ const styles = {
   dashboard: {
     padding: 25,
   },
+  '@global': {
+    body: {
+      margin: 0,
+      padding: 0,
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        'sans-serif'
+      ],
+      '-webkit-font-smoothing': 'antialiased',
+      '-moz-osx-font-smoothing': 'grayscale',
+    },
+    code: {
+      fontFamily: [
+        'source-code-pro',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Courier New',
+        'monospace',
+      ],
+    }
+  },
 }
 
 export default injectSheet(styles)(App)
